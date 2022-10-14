7c06b4cc6cab59e36622c79a48e1b9cee3e7a19e
Caner K <86171865+Cankal03@users.noreply.github.com>
2022-10-14 11:23:49 +0200

    Adding messagecreate listener and gatewayintents

--- src/bot.js
+++ src/bot.js
@@ -1,16 +1,29 @@
 require('dotenv').config();
 const { token, databaseToken } = process.env;
 const { connect } = require('mongoose');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const fs = require('fs')
 
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.MessageContent] });
 client.commands = new Collection();
 client.buttons = new Collection();
 client.selectMenus = new Collection();
 client.modals = new Collection();
 client.commandArray = [];
+
+client.on('messageCreate', async message =>{
+    if(message.content.includes('<@1000488729565208606>')){
+        let embed = new EmbedBuilder()
+        .setDescription(`${client.user.tag} only supports gloabl (/) commands, try /help`)
+        .setColor('Green')
+
+        message.reply({
+            embeds:[embed]
+        })
+    }
+ 
+})
 
 const functionsFolders = fs.readdirSync('./src/functions');
 for (const folder of functionsFolders) {
@@ -22,12 +35,10 @@ for (const folder of functionsFolders) {
 }
 
 
-
 client.handleEvents();
 client.handleCommands();
 client.handleComponents();
 client.login(token);
 (async () => {
     await connect(databaseToken).catch(console.error);
-
 })();
