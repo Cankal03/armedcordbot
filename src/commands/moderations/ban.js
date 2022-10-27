bad30bcb3e1142fb1e5ce738d700b099e7f07323
Caner K <86171865+Cankal03@users.noreply.github.com>
2022-10-27 23:24:51 +0200

    fixed trycatch errors and adding purge messages

---
+++ src/commands/moderations/ban.js
@@ -0,0 +1,46 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+   data: new SlashCommandBuilder()
+      .setName('ban')
+      .setDescription('Ban user')
+      .addUserOption(option => option.setName('target').setDescription('The user to ban').setRequired(true))
+      .addIntegerOption(option =>
+         option.setName('delete_messages')
+            .setDescription('How much of their recent messages hisotry to delete')
+            .setRequired(true)
+            .addChoices(
+               { name: `Don't delete any`, value: 0 },
+               { name: 'Previous 24 Hours', value: 1 },
+               { name: 'Previous 7 Days', value: 7 }))
+      .addStringOption(option => option.setName('reason').setDescription('The reason for banning, if any'))
+      .setDMPermission(false)
+      .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+   async execute(interaction, client) {
+      const user = interaction.options.getUser('target');
+      const reason = interaction.options.getString('reason') || "No reason given!"
+      const delete_messagesdays = interaction.options.getInteger('delete_messages')
+      const member = await interaction.guild.members.fetch(user.id).catch(console.error);
+
+      try {
+         await member.ban({
+            deleteMessagesDays: delete_messagesdays,
+            reason: reason
+         })
+
+         await interaction.reply({
+            content: `<a:US_yes:970033604581265468> | ${member.user.tag} has been banned with the reason of ${reason}`
+         })
+
+         await user.send({
+            content: `You banned from ${interaction.guild.name}`
+         })
+      } catch (error) {
+         if (error) {
+            interaction.reply({
+               content: `<a:US_no:970033684835106868> | Failed to ban the user.`
+            })
+         }
+      }
+   }
+}
---
+++ src/commands/moderations/timeout.js
@@ -0,0 +1,65 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+/*const durations = ({name:'timeout_60secs', value: 60* 1000}, 
+{name:'timeout_5mins', value: 5* 60* 1000}, 
+{name:'timeout_10mins', value: 10* 60* 1000})*/
+
+module.exports = {
+   data: new SlashCommandBuilder()
+      .setName('timeout')
+      .setDescription('Timeout user')
+      .addUserOption(option => option.setName('target').setDescription('The user to timeout').setRequired(true))
+      .addIntegerOption(option =>
+         option.setName('duration')
+            .setDescription('How long they should be timed out for')
+            .setRequired(true)
+            .addChoices(
+               { name: '60 secs', value: 60 * 1000 }, //60.000
+               { name: '5 mins', value: 5 * 60 * 1000 }, //300.000
+               { name: '10 mins', value: 10 * 60 * 1000 }, //600.000
+               { name: '30 mins', value: 30 * 60 * 1000 }, //1.800.000
+               { name: '1 hour', value: 60 * 60 * 1000 }, //3.600.000
+               { name: '1 day', value: 24 * 60 * 60 * 1000 }, //86.400.000
+               { name: '1 week', value: 7 * 24 * 60 * 60 * 1000 })) //604.800.000
+      .addStringOption(option => option.setName('reason').setDescription('The reason for timing them out, if any'))
+      .setDMPermission(false)
+      .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+
+   async execute(interaction, client) {
+      const user = interaction.options.getUser('target');
+      const reason = interaction.options.getString('reason') || "No reason given!"
+      const duration = interaction.options.getInteger('duration')
+      const member = await interaction.guild.members.fetch(user.id)
+
+      
+      try {
+         await member.timeout(duration, reason)
+         
+         await interaction.reply({
+            content: `<a:US_yes:970033604581265468> | ${member.user.tag} has been timed out with reason of ${reason}`
+         })
+
+         await user.send({
+            content: `You timeouted from ${interaction.guild.name}`
+         })
+      } catch (error) {
+         if (error) {
+            interaction.reply({
+               content: `<a:US_no:970033684835106868> | Failed to timeout the user. Discord-Api: ${error}`
+            })
+         }
+      }
+
+      /*console.log(duration)*/
+      /*console.log({user, reason, duration, member})*/
+
+      /*await member.ban({
+       deleteMessagesDays: 1,
+       reason: reason
+      }).catch(console.error)
+
+      await user.send({
+       content: `You banned from ${interaction.guild.name}`
+      }).catch(console.log(`user's dm's are off.`))*/
+   }
+
+}
