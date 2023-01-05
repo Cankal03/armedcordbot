1c1803d1e7bbac9824872b5793b282418f162caa
Caner <86171865+Cankal03@users.noreply.github.com>
2023-01-05 21:26:54 +0100

    Delete src directory

--- src/schemas/ticketcreate_command.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose');
-const modmailSchema = new Schema({
-    UserId: String,
-    ChannelId: String,
-    Reason: String
-})
-
-module.exports = model('Ticketcreate', modmailSchema, 'ticketcreate')
